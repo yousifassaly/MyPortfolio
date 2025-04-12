40804e31e1552242a1e1dffeefe0a58bff5e16e4
YousifAssaly <yousifalassaly01@gmail.com>
2025-04-12 16:46:18 -0700

    update

--- src/pages/About me/AboutMe.jsx
+++ src/pages/About me/AboutMe.jsx
@@ -4,6 +4,9 @@ import { RiHtml5Line } from "react-icons/ri";
 import { IoLogoCss3 } from "react-icons/io5";
 import { TbBrandJavascript } from "react-icons/tb";
 import { FaReact } from "react-icons/fa";
+import { MdOutlineSecurity } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaAws } from "react-icons/fa6";
 
 
 
@@ -23,28 +26,28 @@ export default function About () {
                         <div className="about_cards">
                             
                             <article className="about_card">
-                                <RiHtml5Line className="about_ icon" />
-                                <h5>HTML5 & CSS</h5>
-                                <small>Front End</small>
+                                <FaLaptopCode className="about_ icon" />
+                                <h5>Coding</h5>
+                                <small>HTML, CSS , JavaScript , React</small>
                             </article>
 
                             <article className="about_card">
-                                <TbBrandJavascript className="about_ icon" />
-                                <h5>JavaScript & React</h5>
-                                <small>Coding </small>
+                                <FaAws className="about_ icon" />
+                                <h5>AWS Cloud</h5>
+                                <small>AWS Cloud Practitioner Certification </small>
                             </article>
                             
                             <article className="about_card">
-                                <IoLogoCss3 className="about_ icon" />
-                                <h5></h5>
-                                <small>Front End</small>
+                                <MdOutlineSecurity className="about_ icon" />
+                                <h5>CompTIA</h5>
+                                <small>CompTIA Security+ Certification</small>
                             </article>
 
 
                             <article className="about_card">
                                 <FaReact className="about_ icon" />
                                 <h5>Python</h5>
-                                <small>Coding</small>
+                                <small>Coding And Automation</small>
                             </article>
                             
 
@@ -52,13 +55,11 @@ export default function About () {
                         </div>
 
                         <p>
-                        Hello! I'm Yousif, an aspiring software developer with a passion for building and exploring new technologies.
-                        Currently, I am honing my skills in HTML,CSS, React, JavaScript, Node.js, And still improving for more!
-                        and I am eager to apply my knowledge in practical projects. I thrive on challenge and continuous learning,
-                        and I am always on the lookout for opportunities to innovate and enhance my abilities.
-                        My goal is to develop software solutions that are not only effective but also user-friendly and impactful. 
-                        Whether working on individual projects or collaborating in teams,
-                        I bring a detail-oriented and problem-solving approach to ensure we achieve the best outcomes. Let's connect and create something amazing together!
+                        Hello! I’m Yousif, an aspiring cybersecurity professional with a solid foundation in both security and coding. Alongside my cybersecurity skills in network security, incident response, and cloud security,
+                        I bring hands-on coding experience with HTML, CSS, JavaScript, React, and Python, which helps me understand and secure web applications more effectively. 
+                        Through personal projects, home labs, and platforms like TryHackMe, I’ve worked with tools like Nmap, Wireshark, and Burp Suite to perform vulnerability assessments and web application security testing.
+                        I’m passionate about continuous learning and enjoy tackling challenges that push me to grow. Whether it’s analyzing threats or understanding the code behind web technologies, I bring curiosity and a problem-solving mindset to every opportunity. 
+                        I’m excited to keep improving and contribute to building safer digital environments. Let’s connect and learn together!
                         </p>
 
                             <a href="#Contact" className="btn btn-primary">Let's Connect</a>
